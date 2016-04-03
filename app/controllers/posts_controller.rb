53cb719c9508f96d6c1564e140c473863a5604dd
yoat42 <yoat42@gmail.com>
2016-04-02 23:36:00 -0400

    Added an image asset and linked it on the new posts form, which is now visible after disabling the login check on the posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   #->Prelang (scaffolding:rails/scope_to_user)
-  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+  #before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
   before_action :set_post, only: [:show, :edit, :update, :destroy, :vote]
 
